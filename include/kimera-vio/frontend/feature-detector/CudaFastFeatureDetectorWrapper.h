409d25143a3ddf021334d715d62b09a68c48f084
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-12-18 11:58:02 +0800

    adopt to cuda environment

---
+++ include/kimera-vio/frontend/feature-detector/CudaFastFeatureDetectorWrapper.h
@@ -0,0 +1,21 @@
+//
+// Created by boss on 2020-12-17.
+//
+
+#ifndef KIMERA_VIO_CUDAFASTFEATUREDETECTORWRAPPER_H
+#define KIMERA_VIO_CUDAFASTFEATUREDETECTORWRAPPER_H
+
+#include <opencv2/features2d.hpp>
+#include <opencv2/cudafeatures2d.hpp>
+
+class CudaFastFeatureDetectorWrapper : public cv::Feature2D{
+public:
+    explicit CudaFastFeatureDetectorWrapper(int threshold=10, bool nonmaxSuppression=true, int type=cv::FastFeatureDetector::TYPE_9_16, int max_npoints=5000);
+    void detect(cv::InputArray image, std::vector<cv::KeyPoint> &keypoints, cv::InputArray mask = cv::noArray()) override;
+
+private:
+    cv::Ptr<cv::cuda::Feature2DAsync> internal_detector;
+};
+
+
+#endif //KIMERA_VIO_CUDAFASTFEATUREDETECTORWRAPPER_H
